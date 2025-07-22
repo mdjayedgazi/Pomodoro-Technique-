9cdca26302154744bb6e069cf60d829273f56236
Md.Jayed Gazi <mdjayedgazi@gmail.com>
2025-07-23 02:01:38 +0600

    one line chenge

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def start_timer():
     work_sec = WORK_MIN * 60
     short_break_sec = SHORT_BREAK_MIN * 60
     long_break_sec = LONG_BREAK_MIN * 60
-    # if is's the 1st/3rd/5th/7th rep:
+
     if reps % 8 == 0:
         count_down(long_break_sec)
         label.config(text= "Break", fg= RED)
